1ed440b31507e24133ab276d40bcb788885f140d
metekaya <metekaya55@gmail.com>
2023-12-31 17:57:35 +0300

    Remove static tasks from App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="d-flex justify-content-center">
-      <TaskBoard :tasks="tasks" />
+      <TaskBoard />
     </div>
   </div>
 </template>
@@ -13,16 +13,6 @@ export default {
   name: 'App',
   components: {
     TaskBoard,
-  },
-  data() {
-    return {
-      tasks: [
-        { id: 1, title: 'Task 1', description: 'Description for Task 1', status: 'open' },
-        { id: 2, title: 'Task 2', description: 'Description for Task 2', status: 'testing' },
-        { id: 3, title: 'Task 3', description: 'Description for Task 3', status: 'done' },
-        { id: 4, title: 'Task 4', description: 'Description for Task 4', status: 'unknown' },
-      ],
-    };
   },
 };
 </script>
