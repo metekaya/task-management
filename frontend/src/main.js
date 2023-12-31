f9445499476efba77dd1f9c2df2a7fbd276f6140
metekaya <metekaya55@gmail.com>
2024-01-01 00:48:37 +0300

    Configure bootstrap-vue-next to use bootstrap components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
+import BootstrapVueNext from "bootstrap-vue-next";
 
-createApp(App).mount("#app");
+createApp(App).use(BootstrapVueNext).mount("#app");
