c51ae0f28631e5864d9b66ad7d1b5697bc0ed75f
metekaya <metekaya55@gmail.com>
2024-01-01 19:45:47 +0300

    Add a test for AddTask post request.

--- frontend/tests/unit/TaskBoard.spec.js
+++ frontend/tests/unit/TaskBoard.spec.js
@@ -3,6 +3,7 @@ import MockAdapter from "axios-mock-adapter";
 import { flushPromises, mount, shallowMount } from "@vue/test-utils";
 import TaskBoard from "@/components/TaskBoard.vue";
 import TaskGroup from "@/components/TaskGroup.vue";
+import AddTaskModal from "@/components/AddTaskModal.vue";
 
 describe("TaskBoard.vue", () => {
   const mockedTasks = [
@@ -52,6 +53,64 @@ describe("TaskBoard.vue", () => {
     expect(wrapper.text()).toContain("Mocked Task 1");
     expect(wrapper.text()).toContain("Mocked Task 2");
     expect(wrapper.text()).toContain("Mocked Task 3");
+
+    mockedAxios.restore();
+  });
+
+  it("Adds a task when add-task event is emitted", async () => {
+    const mockedAxios = new MockAdapter(axios);
+
+    mockedAxios
+      .onGet("http://localhost:5000/tasks")
+      .reply(200, { tasks: mockedTasks });
+
+    mockedAxios.onPost("http://localhost:5000/tasks").reply(200, {
+      id: 5,
+      title: "New Task",
+      description: "New Task Description",
+      status: "open",
+    });
+
+    mockedAxios.onGet("http://localhost:5000/tasks").reply(200, {
+      tasks: [
+        ...mockedTasks,
+        {
+          id: 5,
+          title: "New Task",
+          description: "New Task Description",
+          status: "open",
+        },
+      ],
+    });
+
+    const wrapper = mount(TaskBoard);
+    await flushPromises();
+
+    const addTaskModal = wrapper.findComponent(AddTaskModal);
+
+    await addTaskModal.vm.$emit("add-task", {
+      title: "New Task",
+      description: "New Task Description",
+      status: "open",
+    });
+    await wrapper.vm.$nextTick();
+
+    expect(mockedAxios.history.post.length).toBe(1);
+    expect(mockedAxios.history.post[0].url).toBe("http://localhost:5000/tasks");
+    expect(JSON.parse(mockedAxios.history.post[0].data)).toMatchObject({
+      title: "New Task",
+      description: "New Task Description",
+      status: "open",
+    });
+
+    expect(mockedAxios.history.get.length).toBe(2);
+
+    expect(wrapper.vm.tasks).toContainEqual({
+      id: 5,
+      title: "New Task",
+      description: "New Task Description",
+      status: "open",
+    });
 
     mockedAxios.restore();
   });
