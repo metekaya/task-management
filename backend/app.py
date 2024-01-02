68e26d499ad6ddb2f8ffa873abf941814cc08570
metekaya <metekaya55@gmail.com>
2024-01-03 01:58:07 +0300

    Dockerize the app.

--- backend/app.py
+++ backend/app.py
@@ -71,4 +71,4 @@ class TaskListResource(Resource):
 api.add_resource(TaskListResource, '/tasks', '/tasks/<int:task_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
